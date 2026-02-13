091bac354153acfd433f3f330703e0aedb46f9c5
satya7288 <nvvsn9988@gmail.com>
2026-02-13 22:28:34 +0530

    first commit

---
+++ src/test/java/group1/LoginTest.java
@@ -0,0 +1,38 @@
+package group1;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoginTest {
+
+    @Test
+    public void testLogin() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://www.saucedemo.com/");
+
+        WebElement username = driver.findElement(By.id("user-name"));
+        WebElement password = driver.findElement(By.id("password"));
+        WebElement loginBtn = driver.findElement(By.id("login-button"));
+
+        username.sendKeys("standard_user");
+        password.sendKeys("secret_sauce");
+        loginBtn.click();
+
+        assertEquals("Swag Labs", driver.getTitle());
+
+        driver.quit();
+    }
+}
